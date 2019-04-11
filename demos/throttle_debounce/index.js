1131193bc87dc1f9d4c1230a3a4e52a25bf6b19e
weiju.cao <caoweiju@foxmail.com>
2019-04-11 18:06:58 +0800

    fix:修改节流防抖函数

---
+++ demos/throttle_debounce/index.js
@@ -0,0 +1,102 @@
+/*
+ * @Author: weiu.cao
+ * @Date: 2019-04-11 18:03:42
+ * @Last Modified by: weiju.cao
+ * @Last Modified time: 2019-04-11 18:06:21
+*/
+
+/**
+ * throttle
+ * @param {function} handerFn 
+ * @param {number} timeout 
+ */
+const throttle = (handerFn, timeout) => {
+    if(typeof handerFn !== 'function') {
+        return handerFn;
+    }
+    typeof timeout !== 'number' && (timeout=250);
+    let last;
+    return function(...args) {
+        let _now = +new Date();
+        if(last && _now - last < timeout) {
+            handerFn.throttleId && clearTimeout(handerFn.debounceId);
+            handerFn.debounceId = setTimeout(()=>{
+                last = _now;
+                handerFn.apply(this, args);
+            }, timeout);
+        } else {
+            last = _now;
+            handerFn.apply(this, args);
+        }
+    };
+}
+
+/**
+ * throttle 节流函数
+ * @param {fucntion} handerFn 
+ * @param {number} timeout 
+ */
+function throttle(handerFn, timeout) {
+    if(typeof handerFn !== 'function') {
+        return handerFn;
+    }
+    typeof timeout !== 'number' && (timeout=250);
+    var last;
+    return function() {
+        var _now = +new Date(),
+            self = this,
+            args = arguments;
+        if(last && _now - last < timeout) {
+            handerFn.throttleId && clearTimeout(handerFn.debounceId);
+            handerFn.debounceId = setTimeout(function() {
+                last = _now;
+                handerFn.apply(self, args);
+            }, timeout);
+        } else {
+            last = _now;
+            handerFn.apply(self, args);
+        }
+    };
+}
+
+/*******************************************************/
+/******************----------------*********************/
+/*******************************************************/
+
+/**
+ * debounceFn
+ * @param {function} handerFn 代理函数
+ * @param {number} timeout 超时时间 ms
+ */
+const debounceFn = (handerFn, timeout=250) => {
+    if(typeof handerFn !== 'function') {
+        return handerFn;
+    }
+    typeof timeout !== 'number' && (timeout=250);
+    return function(...args) {
+        handerFn.debounceId && clearTimeout(handerFn.debounceId);
+        handerFn.debounceId = setTimeout(()=>{
+            handerFn.apply(this, args);
+        }, timeout);
+    };
+};
+
+/**
+ * debounceFn
+ * @param {function} handerFn 
+ * @param {number} timeout 
+ */
+function debounceFn(handerFn, timeout){
+    if(typeof handerFn !== 'function') {
+        return handerFn;
+    }
+    typeof timeout !== 'number' && (timeout=250);
+    return function() {
+        var self = this,
+            args = arguments;
+        handerFn.debounceId && clearTimeout(handerFn.debounceId);
+        handerFn.debounceId = setTimeout(function() {
+            handerFn.apply(self, args);
+        }, timeout);
+    };
+};
